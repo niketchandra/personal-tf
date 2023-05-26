b35a13228d1f0546081f64b850fc7042ae826920
Niket Chandrawanshi <niket.chandrawanshi@outlook.com>
2023-05-26 20:43:02 +0530

    Create main.tf
    terraform module to launch ubuntu ec2 and run apt-get update and apt-get install docker.io, and run docker run -d --name nginx -p 8080:80 nginx: latest commands in the ec2 allow 8080 in the security group

---
+++ ec2-custom-commands/main.tf
@@ -0,0 +1,30 @@
+# main.tf
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "security_group_name" {
+  description = "Name for the security group"
+  default     = "nginx-security-group"
+}
+
+variable "key_pair_name" {
+  description = "Name of the EC2 key pair"
+  default     = "my-key-pair"
+}
+
+module "ec2_with_docker" {
+  source = "path/to/ec2_with_docker"
+
+  aws_region          = var.aws_region
+  instance_type       = var.instance_type
+  security_group_name = var.security_group_name
+  key_pair_name       = var.key_pair_name
+}
